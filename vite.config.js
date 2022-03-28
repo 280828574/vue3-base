6cbbb5a1e89401fbcefc3d0ac364708c9b3d5c1d
hanleng <hanl@laizone.com>
2022-03-28 20:57:42 +0800

    新增代理配置

--- vite.config.js
+++ vite.config.js
@@ -57,5 +57,14 @@ export default defineConfig({
     port: 9527, // 设置服务启动端口号
     open: true, // 设置服务启动时是否自动打开浏览器
     cors: true, // 允许跨域
+    // 设置代理，根据我们项目实际情况配置
+    proxy: {
+      '/api': {
+        target: '', // 后台服务地址
+        changeOrigin: true, // 是否允许不同源
+        secure: false, // 支持https
+        logLevel: 'debug',
+      },
+    },
   },
 });
