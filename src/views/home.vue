db16481f8068bacb4b351d9ba0099b070b26c946
“hanleng” <hanleng@treyo.com>
2025-11-03 17:22:18 +0800

    Enhance home.vue by adding script setup to access proxy and log $scripts for debugging purposes

--- src/views/home.vue
+++ src/views/home.vue
@@ -2,6 +2,10 @@
   <div>11111111111</div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup>
+  let proxy = getCurrentInstance().proxy
+  const $scripts = proxy.$scripts
+  console.log('%c [ $scripts ]-8', 'font-size:13px; background:pink; color:#bf2c9f;', $scripts)
+</script>
 
 <style scoped></style>
